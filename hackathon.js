378a085dd080609b9f4e9c3462612a19ec58d2eb
Joe Crawley <joe.crawley10@gmail.com>
2012-10-20 11:16:57 -0700

    search bar length

---
+++ hackathon.js
@@ -0,0 +1,8 @@
+
+  (function() {
+    var cx = '004461211522308811956:cbsrrlswngg';
+    var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getEl=ementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+  })();
